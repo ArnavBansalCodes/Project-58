4b17ae37412ce2b6056f0f4a1ffa90942363aa22
ArnavBansalCodes <89875865+ArnavBansalCodes@users.noreply.github.com>
2021-09-06 16:10:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+function arch() {
+    document.getElementById("img2").src = "https://vignette.wikia.nocookie.net/minecraftdungeons/images/c/c7/Archillager.png/revision/latest?cb=20200422151326";
+}
+
+
+
+function witch() {
+    document.getElementById("img2").src = "https://minecraftdungeons.wiki.fextralife.com/file/Minecraft-Dungeons/witch-enemy-minecraft-dungeons-wiki-guide-200px.png";
+}
+
+function skel() {
+    document.getElementById("img2").src = "https://minecraftdungeons.wiki.fextralife.com/file/Minecraft-Dungeons/skeleton-enemy-minecraft-dungeons-wiki-guide-200px.png";
+}
+
+function zomb() {
+    document.getElementById("img2").src = "https://minecraftdungeons.wiki.fextralife.com/file/Minecraft-Dungeons/zombie-enemy-minecraft-dungeons-wiki-guide-200px.png";
+}
+
+function spider() {
+    document.getElementById("img2").src = "https://i.redd.it/omxy6o5efse31.png";
+}
+
+function creep() {
+    document.getElementById("img2").src = "https://minecraftdungeons.wiki.fextralife.com/file/Minecraft-Dungeons/creeper-enemy-minecraft-dungeons-wiki-guide-200px.png";
+}
+
+function necro() {
+    document.getElementById("img2").src = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/5d/Necromancer.png/revision/latest?cb=20210311140329";
+}
+
+function slime() {
+    document.getElementById("img2").src = "https://minecraftdungeons.wiki.fextralife.com/file/Minecraft-Dungeons/slime-enemy-minecraft-dungeons-wiki-guide-200px.png";
+}
+
+function pill() {
+    document.getElementById("img2").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTshyXzmd4ZKTsSpPpxXsAg8YOn3WpA3yQYjUcIV-1tMhxtooMiBYo7AGhQIz5aBIcH_Ts&usqp=CAU";
+}
+
+function vind() {
+    document.getElementById("img2").src = "https://target.scene7.com/is/image/Target/GUEST_4de82c6f-f268-45a5-bc4c-20491c28c855?wid=488&hei=488&fmt=pjpeg";
+}
+
+function ender() {
+    document.getElementById("img2").src = "https://img.favpng.com/24/5/9/minecraft-enderman-game-mob-clip-art-png-favpng-Su6PhpksA3zdVCggs7b6wCdgQ.jpg";
+}
+
+
+function vill() {
+    document.getElementById("img2").src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/359fe050-3abb-4764-b470-33c7883ce044/de8zzyv-f079012c-b274-4576-97da-e8f37a0cb948.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzM1OWZlMDUwLTNhYmItNDc2NC1iNDcwLTMzYzc4ODNjZTA0NFwvZGU4enp5di1mMDc5MDEyYy1iMjc0LTQ1NzYtOTdkYS1lOGYzN2EwY2I5NDgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.uawlq4QewpT_Ilsbr6K2K1Hw1o9uZK5xs1xFQJmPTsY";
+}
